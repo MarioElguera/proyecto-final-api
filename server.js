602f74d8d04714499fe8b7e0e3de74a604c250ea
MarioElguera <marioelguera63@alumnos.cei.es>
2025-04-20 23:28:59 +0200

    refactor code

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-
+// src/server.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/connectDB');
 
+// Rutas
 const authRoutes = require('./routes/authRoutes');
 const articleRoutes = require('./routes/articleRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 
+// Middlewares
 const { errorHandler } = require('./middlewares/errorMiddleware');
-const connectDB = require('./config/connectDB');
 
+// Configuraciones iniciales
 dotenv.config();
 const app = express();
 
-// Middleware
+// Conectar a la base de datos
+connectDB();
+
+// Middlewares generales
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
-app.use(
-    cors({
-        origin: process.env.FRONTEND_URL,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"]
-    })
-);
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Rutas
 app.get('/', (req, res) => {
@@ -34,11 +39,8 @@ app.use('/articles', articleRoutes);
 app.use('/comments', commentRoutes);
 app.use('/events', eventRoutes);
 
+// Middleware de manejo de errores
 app.use(errorHandler);
 
-// Conexión a la base de datos
-connectDB();
-
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+// Exportar la app para que Vercel pueda usarla
+module.exports = app;
